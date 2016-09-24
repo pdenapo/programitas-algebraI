95d25337353c8566b8cdd67addc74701070adaa2
Pablo De Nápoli <pdenapo@gmail.com>
2016-09-24 12:30:46 -0300

    Le agregamos argumentos con argparse. Renombramos la función principal como mcd_con_combinacion_lineal

--- Euclides_con_combinacion_lineal.py
+++ Euclides_con_combinacion_lineal.py
@@ -1,7 +1,10 @@
+#!/usr/bin/env python3
+
 # Programa en Python 3 que calcula el máximo común divisor 
 # usando el algoritmo de Euclides, y los coeficientes que permiten 
 # escribirlo como combinación lineal
 
+import argparse
 
 def chequea_invariante(a,b,alfa_a_b,beta_a_b,mcd_a_b):
  # chequea el invariante del algoritmo 
@@ -10,23 +13,26 @@ def chequea_invariante(a,b,alfa_a_b,beta_a_b,mcd_a_b):
   print("mcd(",a,",",b,")=",mcd_a_b)
   print(mcd_a_b,"=",alfa_a_b,"*",a,"+",beta_a_b,"*",b)
   
-
-
-def ecl(a,b):
+def mcd_con_combinacion_lineal(a,b):
   if b>a:
-   return ecl(b,a)
+   return mcd_con_combinacion_lineal(b,a)
   if b==0:
       alfa_a_b=1
       beta_a_b=0
       mcd_a_b=a
   else: 
      q,r=divmod(a,b)
-     alfa_b_r, beta_b_r, mcd_b_r = ecl(b,r) 
+     alfa_b_r, beta_b_r, mcd_b_r = mcd_con_combinacion_lineal(b,r) 
      alfa_a_b =  beta_b_r
      beta_a_b = alfa_b_r - beta_b_r * q
      mcd_a_b = mcd_b_r
   chequea_invariante (a,b,alfa_a_b,beta_a_b,mcd_a_b)
   return (alfa_a_b,beta_a_b,mcd_a_b)
   
+parser = argparse.ArgumentParser(description='Calcula el máximo común divisor usando el algoritmo de Euclides y lo escibe como una combinación lineal')
+parser.add_argument("a", type=int)
+parser.add_argument("b", type=int)
+args=parser.parse_args()
 
-ecl(360,28)
+print("Calculamos el máximo común divisor entre ",args.a," y ",args.b)   
+mcd_con_combinacion_lineal(args.a,args.b)
