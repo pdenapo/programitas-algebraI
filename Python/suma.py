bfdacd613e85fc861cf29e41b2b6566d8fbb457f
Pablo De Nápoli <pdenapo@gmail.com>
2020-05-08 20:11:53 -0300

    Un ejercicio de la práctica 3

---
+++ Python/suma.py
@@ -0,0 +1,9 @@
+import scipy
+from scipy import special 
+from scipy.special import binom
+
+def s(n):
+    return sum([scipy.special.binom(2*n+1,k) for k in range(0,n+1)])
+
+for n in range (1,10):
+  print ("n=",n,"s(n)=",s(n),"nos dio=", 2**(2*n))
