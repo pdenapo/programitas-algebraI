b8791c07b12417fe6c04bd2ee6b5bb326ae1c270
Pablo De Nápoli <pdenapo@gmail.com>
2020-06-05 15:00:05 -0300

    Usamos docstring y __main__ conforme a las convenciones usales en Python.

--- Python/2020/mcd_y_mcm_factorizando.py
+++ Python/2020/mcd_y_mcm_factorizando.py
@@ -1,13 +1,13 @@
 
-# Dados d y n calcula el máximo exponente de d que divide a n
-# Si d es primo, es el exponente factorización de n
-# (valuación p-ádica)
-# devuelve: el exponente e y el número m=n/d^e
 
 import argparse 
 import sys
 
 def exponente(n,d):
+    """ Dados d y n calcula el máximo exponente de d que divide a n
+        Si d es primo, es el exponente factorización de n
+        (valuación p-ádica)
+        devuelve: (e,m) donde e es el exponente e y  m=n/d^e"""
     q,r = divmod(n,d)
     if r == 0:
       e1,m =  exponente(q,d)
@@ -23,20 +23,17 @@ def exponente(n,d):
        sys.exit("¡No se cumple!")
     return e,m 
 
-# Dada una lista de pares de la forma (p,e) calcula el producto \prod p^d 
 def calcular_producto(l):
+    """ Dada una lista de pares de la forma (p,e) calcula el producto de los valores p^d"""
     p=1
     for f in l:
       p=p*f[0]**f[1]
     return p  
 
 
-# Factoriza n en primos. 
-# Devuelve una lista de pares (p,e) de primos y exponentes
-# El invariante es: n es igual al producto de los primos a los exponentes
-
-
 def factorizar(n):
+    """# Factoriza n en primos. Devuelve una lista de pares (p,e) de primos y exponentes
+    El invariante es: n es igual al producto de los primos a los exponentes"""
     lista = [] 
     if n>1:
         # Miramos los divisores hasta la raiz cuadrada
@@ -62,6 +59,7 @@ def factorizar(n):
     return lista
 
 def mostrar_lindo(l):
+    """Muestra una factorización calculada en forma linda (producto de factores p^e) """
     hay_un_factor= False
     for f in l:
        if hay_un_factor:
@@ -70,8 +68,8 @@ def mostrar_lindo(l):
        hay_un_factor=True
     print("\n")
 
-# busca el exponente de un primo en una factorización dada
 def busca_exponente(p,l):
+    """busca el exponente de un primo en una factorización dada"""
     for f in l:
         if f[0]==p:
            return f[1]
@@ -80,6 +78,8 @@ def busca_exponente(p,l):
 
 
 def mcd_Euclides(a,b):
+  """Calcula el máximo común divisor por el alogitmo de Euclides. Lo usamos como
+  comprobación. """
   if b>a:
    return mcd_Euclides(b,a)
   if b==0:
@@ -89,6 +89,7 @@ def mcd_Euclides(a,b):
    return mcd_Euclides(b,r)
 
 def factorizacion_mcd(fa,fb):
+    """Calcula la factorización del mcd de a y b a partir de las factorizaciones de a y de b."""
     resultado=[]
     for f in fa:
       p = f[0] # El primo
@@ -101,13 +102,14 @@ def factorizacion_mcd(fa,fb):
     return resultado
 
 def listar_primos(fa):
+    """Lista los primos que aparecen en una factorización dada"""
     resultado=[]
     for f in fa:
       resultado.append(f[0])
     return resultado
 
-# Obtener una lista sin valores repetidos a partir de una dada
 def unicos(lista):
+    """Obtener una lista sin valores repetidos a partir de una dada."""
     resultado = [] 
     for x in lista: 
         if x not in resultado: 
@@ -115,6 +117,7 @@ def unicos(lista):
     return resultado
 
 def factorizacion_mcm(fa,fb):
+    """Calcula la factorización del mcm de a y b a partir de las factorizaciones de a y de b."""
     resultado=[]
     todos_los_primos = listar_primos(fa) + listar_primos(fb)
     todos_los_primos = unicos(todos_los_primos)
@@ -125,31 +128,36 @@ def factorizacion_mcm(fa,fb):
         eb = busca_exponente(p,fb) # el exponente en la otra factorización
         resultado.append((p,max(ea,eb)))
     return resultado
-    
-parser = argparse.ArgumentParser(description='Calcula el máximo común divisor y el máximo común múltiplo factorizando.')
-parser.add_argument("a", type=int)
-parser.add_argument("b", type=int)
-args=parser.parse_args()
 
-print("Calculamos el máximo común divisor y el mínimo común múltiplo entre ",args.a," y ",args.b," usando sus factorizaciones \n")   
-fa = factorizar(args.a)
-fb = factorizar(args.b)
-print(args.a,"=",end="")
-mostrar_lindo(fa)
-print(args.b,"=",end="")
-mostrar_lindo(fb)
-fmcd= factorizacion_mcd(fa,fb)
-mcd = calcular_producto(fmcd)
-# Chequeamos el resultado con el que da el algoritmo de Euclides
-if not(mcd == mcd_Euclides(args.a,args.b)):
+if __name__ == "__main__":
+    # Parte principal del programa. Leemos los argumentos
+    parser = argparse.ArgumentParser(description='Calcula el máximo común divisor y el máximo común múltiplo factorizando.')
+    parser.add_argument("a", type=int)
+    parser.add_argument("b", type=int)
+    args=parser.parse_args()
+
+    print("Calculamos el máximo común divisor y el mínimo común múltiplo entre ",args.a," y ",args.b," usando sus factorizaciones \n")   
+    # factorizamos a y b
+    fa = factorizar(args.a)
+    fb = factorizar(args.b)
+    print(args.a,"=",end="")
+    mostrar_lindo(fa)
+    print(args.b,"=",end="")
+    mostrar_lindo(fb) 
+    # Calculamos el mcd factorizando  
+    fmcd= factorizacion_mcd(fa,fb)
+    mcd = calcular_producto(fmcd)
+    # Chequeamos el resultado con el que da el algoritmo de Euclides
+    if not(mcd == mcd_Euclides(args.a,args.b)):
         sys.exit("El mcd no fue correctamente calculado")        
-print("mcd =",mcd,end=" = ")
-mostrar_lindo(fmcd)
-fmcm= factorizacion_mcm(fa,fb)
-mcm=calcular_producto(fmcm)
-# Chequeamos el resultado con el que da el algoritmo de Euclides
-if not(mcm == args.a*args.b/mcd):
-        sys.exit("El mcd no fue correctamente calculado")        
-print("mcm =",calcular_producto(fmcm),end=" = ")
-mostrar_lindo(fmcm)
+    print("mcd =",mcd,end=" = ")
+    mostrar_lindo(fmcd)
+    fmcm= factorizacion_mcm(fa,fb)
+    # Calculamos el mcm factorizando
+    mcm=calcular_producto(fmcm)
+    # Chequeamos el resultado con el obtenido a partir del mcd
+    if not(mcm == args.a*args.b/mcd):
+        sys.exit("El mcm no fue correctamente calculado")        
+    print("mcm =",calcular_producto(fmcm),end=" = ")
+    mostrar_lindo(fmcm)
 
