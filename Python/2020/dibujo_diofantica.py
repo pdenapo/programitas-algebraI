e6a445dcb82a8f135cb97ca7619460c91b66eb96
Pablo De Nápoli <pdenapo@gmail.com>
2020-06-19 17:20:58 -0300

    Modificaciones al programita para la ecuación diofántica: mostramos los valores de los parámetros.

--- Python/2020/dibujo_diofantica.py
+++ Python/2020/dibujo_diofantica.py
@@ -7,32 +7,52 @@ import sys
 
 def dibuja_diofántica(a,b,c):
     # definimos el tamaño de la imagen
-    plt.figure(figsize=(10,10))
+    plt.figure(figsize=(20,20))
     # Agregamos los ejes y ponemos marcas en los enteros
     plt.axvline()
     plt.axhline()
-    plt.xticks(range(-10,11))
-    plt.yticks(range(-10,11))
+    plt.xticks(range(-20,20))
+    plt.yticks(range(-20,20))
     # dibujamos el retítulo de coordenadas enteras
-    for x in range(-10,11):
-        for y in range(-10,11):
+    for x in range(-20,21):
+        for y in range(-20,21):
             plt.plot(x,y,"ro",markersize=3)
-    x = np.linspace(-10,10)
+    x = np.linspace(-20,20)
     # ax+by=c <=> by= c-ax <=> y=c/b - (a/b)*x
     y = (c/b) - (a/b)*x
-            
+    
     s,t,d = mcd_con_combinacion_lineal(a,b)
     # d es el máximo común divisor de a y b, sa+tb=d
     if not(s *a + t* b == d):
-        sys.exit("¡No se cumple!")
+        sys.exit("¡No se cumple lo que promete el algoritmo de Euclides!")
   
-    q,r = divmod(c,d)
+    c_prima,r = divmod(c,d)
     if r==0:
     # Encontramos una solución particular
-       x0= q*s
-       y0= q*t 
-       plt.plot(x0,y0,"bo")
+       print("s=",s,"t=",t,"c_prima=",c_prima)
+       x0= c_prima*s
+       y0= c_prima*t 
+       if not(x0 *a + y0* b == c):
+            sys.exit("¡No se cumple la condición para ser solución particular !")
+       plt.plot(x0,y0,"mo")
        solución = "Una solución particular es ("+str(x0)+","+str(y0)+")"
+       # coprimalizamos como en la proposición 5.1.6 del apunte de la prof. Krick
+       # Podemos obtener tantas soluciones como querramos agrandando el rango de k
+       a_prima = a/d
+       b_prima = b/d
+       print("d=",d,"a_prima=",a_prima,"b_pirma=",b_prima,"c_prima=",c_prima,"s=",s,"t=",t,)
+       print("Las soluciones son de la forma: (",x0,"+",b_prima,"*k,",y0,"-",a_prima,"*k)")
+       print("Algunas soluciones son:")
+       for k in range (-5,5):
+                xp = x0+b_prima*k
+                yp = y0-a_prima*k
+                if not(xp *a + yp* b == c):
+                    sys.exit("¡No se cumple la condición para solución !")
+                print("(",xp,yp,")")
+                if k!=0:
+                  plt.plot(xp,yp,'bo')
+    else: 
+       solución = "No hay soluciones."
     plt.plot(x,y,"g",label="La ecuación diofántica "+str(a)+"*x+"+str(b)+"*y="+str(c)+"\n"+solución)
     legend = plt.legend(loc='upper center', shadow=True, fontsize='x-large')
     legend.get_frame().set_facecolor('C0')
