e27bd3ee3e0e2b8fdd376a75c57386108958a252
Pablo De Nápoli <pdenapo@gmail.com>
2020-11-12 14:34:06 -0300

    Ejercicio del parcial, corregido.

--- D/ejercicio3_del_parcial.d
+++ D/ejercicio3_del_parcial.d
@@ -2,23 +2,42 @@
 // https://stackoverflow.com/questions/23994066/check-if-array-contains-element-in-d
 
 import std.stdio;
-import std.numeric:gcd;
-import std.algorithm: canFind;
-import std.algorithm.sorting;
+import std.math:abs;
+
+// Calculamos el máximo común divisor por el algoritmo de Euclides
+// Este código funciona bien con int, pero no con ulong
+int gcd(int a, int b)
+{
+    if ((a<0) || (b<0))
+        return gcd(abs(a),abs(b));
+    if (b==0)
+        return a;
+    // Calculamos el cociente y el resto en la división de a por b    
+    else
+    {    
+        int q = a /b;
+        int r = a - b*q;
+        return gcd(b,r);
+    }    
+}
+
+struct Par
+{
+  int a,b;
+}
 
 void main()
 {
-    const ulong limite=1000;
-    ulong[] posibles_valores;
-    for (ulong a=1;a<limite;a++)
-        for (ulong b=1; b<limite; b++)
+    const int limite=1000;
+    Par [int] posibles_valores;
+    for (int a=1;a<limite;a++)
+        for (int b=1; b<limite; b++)
         if (gcd(a,b)==3)
         {
-           ulong d= gcd(11*a-7*b,4*a+3*b);
+           int d= gcd(11*a-7*b,4*a+3*b);
            writeln("a= ",a," b= ",b, " d= ",d);
-           if (!posibles_valores.canFind(d))
-            posibles_valores ~= d;
+           if (!(d in posibles_valores))
+            posibles_valores[d]= Par(a,b);
         }
-    posibles_valores.sort();
     writeln("Posibles valores = ",  posibles_valores);
 }
